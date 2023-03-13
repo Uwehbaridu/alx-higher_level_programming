e2b203023256005f9238aa0331fe18b34c507436
Bariduanen Uweh <uwehbariduanen@gmail.com>
2023-03-13 15:04:03 -0700

    List

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    for n in my_list:
-        if idx < 0:
-            return(my_list)
-        elif idx >= len(my_list):
-            return(my_list)
-        else:
-            my_list[idx] = idx
-            return(my_list)
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
