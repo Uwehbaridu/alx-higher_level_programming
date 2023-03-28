eb0b306a61b091afd53fc125c133341d3e91fd4d
Bariduanen Uweh <uwehbariduanen@gmail.com>
2023-03-28 14:33:12 -0700

    Exception Handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-def safe_print_list(my_list[], x=0):
+def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            count += 1
+            count = count + 1
         except IndexError:
-
-            break
-        print("")
-        return(count)
+            continue
+    print("")
+    return count
