284787ff010de9bd5bd8ccc8a0ba6eb63c30119a
Bariduanen Uweh <uwehbariduanen@gmail.com>
2023-04-12 15:57:44 -0700

    Python inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
-#!/urs/bin/python3
-"""Define an object attribute lookup function."""
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
     """Return a list of an object's available attributes."""
-    return(dir(obj))
+    return (dir(obj))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """Implements sortetd printing for the built-in list class."""
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
         """Print a list in sorted ascending order."""
